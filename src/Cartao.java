892b358ba5b18c212d428357b4cbd3b943d395a0
raiss <raiss@LAPTOP-ERCVA8D5>
2017-12-27 19:00:50 -0200

    First Commit

---
+++ src/Cartao.java
@@ -0,0 +1,9 @@
+import java.time.LocalDateTime;
+
+public class Cartao {
+	private String numero;
+	private String Bandeira;
+	private String nomeCartao;
+	private String codigo;
+	private LocalDateTime validade;
+}
---
+++ src/ContaBancaria.java
@@ -0,0 +1,7 @@
+
+public class ContaBancaria {
+	private String CPF;
+	private String nome;
+	private String numero_conta;
+
+}
