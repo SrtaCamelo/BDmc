66bd3fd358e8ba3b5bd10a36f8fdbd135b7dad92
SrtaCamelo <SrtaCamelo@10.246.214.4>
2018-01-14 20:28:56 -0300

    Nova classe -repositório

---
+++ src/br/ufrpe/repositorios/VoluntariopRep.java
@@ -0,0 +1,45 @@
+package br.ufrpe.repositorios;
+import java.util.ArrayList;
+
+import br.ufrpe.minhacapanha.domain.*;
+
+public class VoluntariopRep {
+	ArrayList<VoluntarioPessoa> volutariopRep;
+	
+	
+	public VoluntariopRep () {
+		
+	}
+	
+	public void add(VoluntarioPessoa nova) {
+		this.volutariopRep.add(nova);
+	}
+	
+	public void remove(VoluntarioPessoa remover) {
+		this.volutariopRep.remove(remover);
+	}
+	
+	public VoluntarioPessoa buscar(int index) {
+		return this.volutariopRep.get(index);
+	}
+	
+	public VoluntarioPessoa buscarCPF(String cpf) {
+		boolean found = false;
+		for(VoluntarioPessoa busca: this.volutariopRep) {
+			if(busca.getCpf().equals(cpf)) {
+				return busca;
+			}
+		}
+		
+	}
+	
+	public void update(VoluntarioPessoa nova) {
+		String cpf = nova.getCpf();
+		VoluntarioPessoa remover = buscarCPF(cpf);
+		remove(remover);
+		add(nova);
+		
+	}
+
+}
+
